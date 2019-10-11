3168ba37eae1b481b8bdf6b4a8d751696ec7f539
cangyu <497481821@qq.com>
2019-10-11 16:04:14 +0800

    首页

--- miniprogram/pages/guide/guide.js
+++ miniprogram/pages/guide/guide.js
@@ -5,9 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    text: '全国版垃圾分类测试上线，分享朋友圈，大家一起比拼！为大力推进垃圾分类，如有平台需接入垃圾查询服务，请留言',
+    marqueePace:1,
+    marqueeDistance:0,
+    size:22,
+    interval:20
   },
-
+  publicity: function () {
+    var that = this;
+    var length = that.data.text.length * than.data.size;//文字宽度
+    var windowWidth = wx.getSystemInfoSync().windowWidth;//屏幕宽度
+    that.setData({
+      length,
+      windowWidth
+    });
+    than.scrollFn();
+  },
+  scrollFn: function () {
+    var that = this;
+    var length = that.data.length;//滚动文字的宽度
+    var windowWidth = that.data.windowWidth;
+    if (length > windowWidth) {
+      var interval = setInterval(function () {
+        var crentleft = that.data.marqueeDistance;
+        if (crentleft < 550) {//判断是否滚动到最大宽度
+          that.setData({
+            marqueeDistance: crentleft + that.data.marqueePace
+          })
+        }
+        else {
+          that.setData({
+            marqueeDistance: 0 // 直接重新滚动
+          });
+          clearInterval(interval);
+          that.scrollFn();
+        }
+      }, that.data.interval);
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
